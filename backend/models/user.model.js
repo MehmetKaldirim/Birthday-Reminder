34fb47f5857e219b85c9016c8d40a9b40a8a9a66
Mehmet <mathdenizi@gmail.com>
2024-07-07 20:13:09 +0200

    signup work properly

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -12,6 +12,13 @@ const userSchema = new mongoose.Schema(
       required: true,
       unique: true,
     },
+    birthday: {
+      type: Date,
+    },
+    gender: {
+      type: String,
+      required: true,
+    },
     password: {
       type: String,
       required: true,
