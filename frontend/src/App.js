a1a8a8029d52343eb5aad8240c18cc7860d43131
Mehmet <mathdenizi@gmail.com>
2024-07-08 18:55:05 +0200

    pages created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,37 @@
-import React, { useState } from 'react'
-import Card from './Card';
-import "./index.css"
-import UpcomingBirthday from './UpcomingBirthday';
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import Dashboard from "./pages/Dashboard";
+import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
+
+import Card from "./Card";
+import "./index.css";
+import UpcomingBirthday from "./UpcomingBirthday";
 
 const App = () => {
-
-
-
   return (
-    <div className='body'>
-    <h1 style={{ color: "lightpink"}}>Today's Birthday</h1>
-    <Card/>
-    <h1 style={{ color: "lightgreen"}}>Upcoming Birthday</h1>
-    <UpcomingBirthday />
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Header />
+      <div className="body">
+        <h1 style={{ color: "lightpink" }}>Today's Birthday</h1>
+        <Card />
+        <h1 style={{ color: "lightgreen" }}>Upcoming Birthday</h1>
+        <UpcomingBirthday />
+      </div>
 
-export default App
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
