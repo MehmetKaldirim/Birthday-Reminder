9fdf987f7b7e2e57e44aabd7988ef050a68ae691
Mehmet <mathdenizi@gmail.com>
2024-07-10 00:09:52 +0200

    footer created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import SignIn from "./pages/SignIn";
 import Dashboard from "./pages/Dashboard";
 import SignUp from "./pages/SignUp";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 import "./index.css";
 
@@ -22,6 +23,7 @@ const App = () => {
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
