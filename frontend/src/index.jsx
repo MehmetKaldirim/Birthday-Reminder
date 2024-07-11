f889467cf88ba5d77c3d3fa63eaf0dbaecd08cca
Mehmet <mathdenizi@gmail.com>
2024-07-12 00:14:37 +0200

    dark light mode shift functionality

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -5,13 +5,16 @@ import App from "./App";
 import { store, persistor } from "./redux/store.js";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import ThemeProvider from "./components/ThemeProvider.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <PersistGate persistor={persistor}>
       <Provider store={store}>
-        <App />
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
       </Provider>
     </PersistGate>
   </React.StrictMode>
