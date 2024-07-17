28f99cd4082b72fb8eed34ab956cac58c3afa31f
Mehmet <mathdenizi@gmail.com>
2024-07-17 02:45:52 +0200

    get all users route created

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,3 +1,32 @@
+import User from "../models/user.model.js";
+
 export const test = (req, res) => {
   res.json({ message: "API is working!" });
+};
+
+export const getusers = async (req, res, next) => {
+  try {
+    const users = await User.find({});
+    const totalUsers = await User.countDocuments();
+
+    const now = new Date();
+
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    const lastMonthUsers = await User.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    res.status(200).json({
+      users,
+      totalUsers,
+      lastMonthUsers,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { test } from "../controllers/user.controller.js";
+import { test, getusers } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get("/test", test);
+router.get("/users", getusers);
 
 export default router;
